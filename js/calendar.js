381745207079ea03551795fd2927370f4b6ace2f
Brandon Harris <bharris@Brandons-MBP.lan>
2019-03-06 22:58:02 -0600

    calendar functionality added but color scheme ugly and in need of fixing

--- js/calendar.js
+++ js/calendar.js
@@ -1,3 +1,52 @@
+var toChange;
+
+function makeCalE(elem)
+{
+  for (i=0;i<elem.classList.length; i++)
+  {
+    if (elem.classList[i] == 'voteday')
+    {
+      elem.classList.remove('voteday');
+      return;
+    }
+    else if (elem.classList[i] == 'mailday')
+    {
+      elem.classList.remove('mailday');
+      return;
+    }
+  }
+  toChange = elem;
+  p = document.getElementById('popupHandle');
+  p.style.display = 'block';
+}
+
+function closePopup()
+{
+  document.getElementById('popupHandle').style.display = 'none';
+}
+
+function submitPopup()
+{
+  if(document.getElementById('voteType').checked)
+  {
+    toChange.classList.add('voteday');
+  }
+  else if(document.getElementById('mailType').checked)
+  {
+    toChange.classList.add('mailday');
+  }
+  closePopup();
+}
+
+window.onclick = function(event)
+{
+  if (event.target == document.getElementById('popupHandle'))
+  {
+    document.getElementById('popupHandle').style.display = 'none';
+  }
+}
+
+/*
 function YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
     // Create a new instance of calendar, placing it in
     // #mycalendar container, setting its width to 340px,
@@ -38,3 +87,4 @@ function YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
       calendar.set('showNextMonth', !(calendar.get("showNextMonth")));
     });
 });
+*/
