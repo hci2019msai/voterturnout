4586fb4c61d941b94233966591e881a6923a776e
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-03-06 21:22:38 -0600

    Hourly updates

--- js/registration1.js
+++ js/registration1.js
@@ -1,6 +1,7 @@
 function toggletest () {
   var x = document.getElementById("main_container");
     var y = document.getElementById("container1B");
+    var z = document.getElementById("container1C");
  if (x.style.display === "none") {
    x.style.display = "grid";
 }
@@ -9,4 +10,17 @@ else {
    x.style.display = "none";
      y.style.display ="grid";
  }
+}
+
+function activate1C () {
+    var oneCx = document.getElementById("main_container");
+    var oneCy = document.getElementById("container1B");
+    var oneCz = document.getElementById("container1C");
+if (oneCy.style.display === "none") {
+    oneCy.style.display = "grid"}
+else {
+    oneCx.style.display = "none";
+    oneCy.style.display = "none";
+    oneCz.style.display = "grid";
+}
 }
