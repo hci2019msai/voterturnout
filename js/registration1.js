0a9f4d46cdea97a006737e339de8c1870d3e32ca
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-03-06 22:42:09 -0600

    ongoing struggles with JS

--- js/registration1.js
+++ js/registration1.js
@@ -5,7 +5,7 @@ function toggletest () {
  if (x.style.display === "none") {
    x.style.display = "grid";
 }
-    
+
 else {
    x.style.display = "none";
      y.style.display ="grid";
@@ -24,3 +24,19 @@ else {
     oneCz.style.display = "grid";
 }
 }
+
+
+function openSection(evt, sectionName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(sectionName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
