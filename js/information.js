5ea01e1a57eca750448e751b4056737fa4429bae
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-03-12 03:11:41 -0500

    Hourly commits

---
+++ js/information.js
@@ -0,0 +1,12 @@
+function toggleStateContent() {
+    var PreToggle = document.getElementById("toggleContainer");
+    var PostToggle = document.getElementById("PostToggle");
+        PostToggle.style.display = "block";
+        PreToggle.style.display = "none";
+    }
+function toggleChoices() {
+    var PreToggle = document.getElementById("toggleContainer");
+    var PostToggle = document.getElementById("PostToggle");
+        PostToggle.style.display = "none";
+        PreToggle.style.display = "block";
+}
