6d66a40a69323ba7e007f6ad36d22509e7539f25
ncaldwell17 <noahcaldwell14@yahoo.com>
2019-03-13 14:12:19 -0500

    in TA time edits

---
+++ js/absentee.js
@@ -0,0 +1,24 @@
+function revealName() {
+    var x = document.getElementById("NameContainer");
+    x.style.display = "grid";
+}
+
+function revealBirth() {
+    var x = document.getElementById("BirthContainer");
+    x.style.display = "grid";
+}
+
+function revealMail() {
+    var x = document.getElementById("MailContainer");
+    x.style.display = "grid";
+}
+
+function revealLicense() {
+    var x = document.getElementById("LicenseContainer");
+    x.style.display = "grid";
+}
+
+function revealNext() {
+    var x = document.getElementById("Next");
+    x.style.display = "grid";
+}
