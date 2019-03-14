9570054a566f7e97ddec597417de2ce8e559b874
Brandon Harris <bharris@Brandons-MBP.lan>
2019-03-14 00:53:58 -0500

    navbar changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("supercontainer").style.marginLeft = "250px";
+//  document.getElementById("supercontainer").style.marginLeft = "250px";
 }
 
 function closeNav() {
